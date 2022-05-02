f7a9afa678bc47aacd58a768266a15cd072a58e7
Mathi03 <mathi031@hotmail.com>
2022-05-02 08:16:05 -0500

    Nvchad contribution

---
+++ plugins/init.lua
@@ -0,0 +1,59 @@
+return {
+   ["windwp/nvim-ts-autotag"] = {
+      ft = { "html", "javascriptreact" },
+      after = "nvim-treesitter",
+      config = function()
+         require("nvim-ts-autotag").setup()
+      end,
+   },
+
+   ["jose-elias-alvarez/null-ls.nvim"] = {
+      after = "nvim-lspconfig",
+      config = function()
+         require("custom.plugins.null-ls").setup()
+      end,
+   },
+
+   ["nvim-telescope/telescope-media-files.nvim"] = {
+      after = "telescope.nvim",
+      config = function()
+         require("telescope").setup {
+            extensions = {
+               media_files = {
+                  filetypes = { "png", "webp", "jpg", "jpeg" },
+               },
+               -- fd is needed
+            },
+         }
+         require("telescope").load_extension "media_files"
+      end,
+   },
+
+   ["Pocco81/TrueZen.nvim"] = {
+      cmd = {
+         "TZAtaraxis",
+         "TZMinimalist",
+         "TZFocus",
+      },
+      config = function()
+         require "custom.plugins.truezen"
+      end,
+   },
+    ["karb94/neoscroll.nvim"] = {
+       config = function()
+          require("neoscroll").setup()
+       end,
+
+       -- lazy loading
+       setup = function()
+         require("core.utils").packer_lazy_load "neoscroll.nvim"
+       end,
+    },
+    ["folke/which-key.nvim"] = {
+      config = function()
+        require("which-key").setup()
+      end,
+      -- event = "BufWinEnter",
+      -- disable = true,
+    },
+}
